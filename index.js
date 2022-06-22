92f4d336374ea6cd4ec4032e9633d75c7309055a
unknown <oladejofavourajoke@gmail.com>
2022-06-22 22:53:31 +0100

    alert functions add

---
+++ index.js
@@ -0,0 +1,38 @@
+const calculator = () => {
+
+    let firstNumber = parseFloat(prompt("Enter a number"));
+    if (isNaN(firstNumber)) {
+        return alert("Invalid Number")
+    };
+
+
+    let operator = prompt("Enter operator");
+    if (operator != '+' && operator != '-' && operator != '*' && operator !== '/') {
+        return alert("Invalid operator")
+    };
+
+    let secondNumber = parseFloat(prompt("Enter another number"));
+    if (isNaN(secondNumber)) {
+        return alert("Invalid Number")
+    };
+
+    switch (operator) {
+        case '+':
+            result = firstNumber + secondNumber;
+            break
+        case '-':
+            result = firstNumber - secondNumber;
+            break
+        case '*':
+            result = firstNumber * secondNumber;
+            break
+        case '/':
+            result = firstNumber / secondNumber;
+            break
+    }
+
+    return alert(`${result}`)
+}
+
+
+calculator()
